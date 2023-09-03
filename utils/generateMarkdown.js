ff63a49b8e72a4a6daeccb8f3bb7a17e3e884903
Rinkal <email@example.com>
2023-09-03 18:34:50 +0100

    added function to generate markdown for README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,42 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  // Generate the badge and license notice based on user's choice
+  const licenseBadge = generateLicenseBadge(data.license);
+  const licenseNotice = generateLicenseNotice(data.license);
+
   return `# ${data.title}
 
-`;
+  ${licenseBadge}
+
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ${licenseNotice}
+  
+  ## Contributing
+  ${data.contributing}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  If you have any questions, feel free to reach out to me at [${data.email}](mailto:${data.email}). You can also find me on GitHub as [${data.github}](https://github.com/${data.github}).
+  `;
 }
 
 module.exports = generateMarkdown;
